389961b79b6fd984f66ff8d3779fe57d4e684c84
Jordan Patton <jordanpatton@gmail.com>
2016-11-17 13:00:13 -0800

    tests

--- test/src/services/selector.spec.js
+++ test/src/services/selector.spec.js
@@ -70,4 +70,108 @@ describe('selector', function () {
 
   });
 
+  describe('selectChildElementByAttribute', function () {
+
+    var attributeName = 'someAttributeName';
+    var attributeValue = 'someAttributeValue';
+
+    it('selects a child element by attribute if a matching child is present', function () {
+      var element = {childNodes: [{}]};
+      element.childNodes[0][attributeName] = attributeValue;
+      var result = selector.__get__('selectChildElementByAttribute')(element, attributeName, attributeValue);
+      expect(result).toEqual(element.childNodes[0]);
+    });
+
+    it('selects the first matching child element if multiple matches are present', function () {
+      var element = {childNodes: [{key1: 'val1'}, {key2: 'val2'}]};
+      element.childNodes[0][attributeName] = attributeValue;
+      element.childNodes[1][attributeName] = attributeValue;
+      var result = selector.__get__('selectChildElementByAttribute')(element, attributeName, attributeValue);
+      expect(result).toEqual(element.childNodes[0]);
+      expect(result).toNotEqual(element.childNodes[1]);
+    });
+
+    it('does not select a non-matching child element', function () {
+      var element = {childNodes: [{}, {}]};
+      element.childNodes[1][attributeName] = attributeValue;
+      var result = selector.__get__('selectChildElementByAttribute')(element, attributeName, attributeValue);
+      expect(result).toNotEqual(element.childNodes[0]);
+      expect(result).toEqual(element.childNodes[1]);
+    });
+
+    it('selects nothing if the element has no child nodes', function () {
+      var element = {childNodes: []};
+      var result = selector.__get__('selectChildElementByAttribute')(element, attributeName, attributeValue);
+      expect(result).toEqual(undefined);
+    });
+
+    it('selects nothing if the element has no matching child nodes', function () {
+      var element = {childNodes: [{}]};
+      element.childNodes[0][attributeName] = 'wrongAttributeValue';
+      var result = selector.__get__('selectChildElementByAttribute')(element, attributeName, attributeValue);
+      expect(result).toEqual(undefined);
+    });
+
+  });
+
+  describe('selectChildElement', function () {
+
+    it('selects a child element by className if the "." prefix is given', function () {
+      var element = {
+        childNodes: [{
+          className: 'some-class',
+          id: 'some-id'
+        }]
+      };
+      var result = selector.selectChildElement(element, ['.some-class']);
+      expect(result).toEqual(element.childNodes[0]);
+    });
+
+    it('selects a child element by id if the "." prefix is not given', function () {
+      var element = {
+        childNodes: [{
+          className: 'some-class',
+          id: 'some-id'
+        }]
+      };
+      var result = selector.selectChildElement(element, ['#some-id']);
+      expect(result).toEqual(element.childNodes[0]);
+    });
+
+    it('selects a nested child element by attribute if a matching child is present', function () {
+      var element = {
+        childNodes: [{
+          className: 'some-class-1',
+          childNodes: [{
+            className: 'some-class-2',
+            childNodes: [{
+              id: 'some-id-3'
+            }]
+          }]
+        }]
+      };
+      var result = selector.selectChildElement(element, ['.some-class-1', '.some-class-2', '#some-id-3']);
+      expect(result).toEqual(element.childNodes[0].childNodes[0].childNodes[0]);
+    });
+
+    it('selects nothing if a matching child is not present', function () {
+      var element = {
+        childNodes: [{
+          className: 'wrong-class'
+        }]
+      };
+      var result = selector.selectChildElement(element, ['.some-class']);
+      expect(result).toEqual(undefined);
+    });
+
+    it('selects nothing if the parent has no child nodes', function () {
+      var element = {
+        childNodes: []
+      };
+      var result = selector.selectChildElement(element, ['.some-class']);
+      expect(result).toEqual(undefined);
+    });
+
+  });
+
 });
