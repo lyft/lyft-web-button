d5aa5cf2afdbaba3ea5e3f309d15904fecd61f2a
Jordan Patton <jordanpatton@gmail.com>
2016-11-17 14:32:52 -0800

    tests

--- test/src/services/jsonp.spec.js
+++ test/src/services/jsonp.spec.js
@@ -21,4 +21,46 @@ describe('jsonp', function () {
     expect(typeof jsonp.__get__('serialize')).toEqual('function');
   });
 
+  describe('request', function () {
+
+    beforeEach(function () {
+      jsonp.__set__('injectScript', expect.createSpy());
+    });
+
+    afterEach(function () {
+      jsonp.__get__('injectScript').reset();
+    });
+
+    it('throws an exception if the options object is undefined', function () {
+      var options = undefined;
+      expect(function () {
+        jsonp.request(options);
+      }).toThrow(TypeError);
+    });
+
+    it('does not throw an exception if the options object is well-defined', function () {
+      var options = {url: 'someUrl'};
+      expect(function () {
+        jsonp.request(options);
+      }).toNotThrow(TypeError);
+    });
+
+    it('injects a script with the given options', function () {
+      var options = {
+        callback: 'someCallback',
+        data: {key2: 'val2'},
+        url: 'someUrl?key1=val1'
+      };
+      jsonp.request(options);
+      expect(jsonp.__get__('injectScript')).toHaveBeenCalledWith({
+        src: (
+          options.url +
+          '&callback=' + options.callback +
+          '&key2=' + options.data.key2
+        )
+      });
+    });
+
+  });
+
 });
